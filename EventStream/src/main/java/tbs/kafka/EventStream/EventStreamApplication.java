ee916f7e22aa2d68a2f8d7ea32017e4d875099f0
thytran38 <ax4409h@gmail.com>
2021-05-26 02:52:51 +0700

    add some services

---
+++ EventStream/src/main/java/tbs/kafka/EventStream/EventStreamApplication.java
@@ -0,0 +1,16 @@
+package tbs.kafka.EventStream;
+
+import org.apache.kafka.streams.StreamsBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventStreamApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EventStreamApplication.class, args);
+
+	}
+
+}
---
+++ EventStream/src/test/java/tbs/kafka/EventStream/EventStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package tbs.kafka.EventStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
