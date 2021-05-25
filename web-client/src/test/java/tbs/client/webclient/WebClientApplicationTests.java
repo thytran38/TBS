1cc5c4e3d69c09ddbc67daa5ab4409fbc6e667d3
thytran38 <ax4409h@gmail.com>
2021-05-26 00:19:37 +0700

    add webclient service

---
+++ web-client/src/test/java/tbs/client/webclient/WebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package tbs.client.webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
